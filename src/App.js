d6ce5c58bdf82cc649ef70aa7b06210a39806730
aEmreAydemir <45498427+aEmreAydemir@users.noreply.github.com>
2021-04-07 01:34:07 +0300

    Update App.js
    Robots' info is now fetched from jsonplaceholder fake api test website instead of users.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
-import {robots} from './robots';
+//import {robots} from './robots';
 import SearchBox from './SearchBox.js'
 import './App.css'
 
@@ -8,9 +8,15 @@ class App extends React.Component {
     constructor() {
         super()
         this.state= {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> {return response.json()})
+        .then(users=>{this.setState({robots:users})})
     }
 
     /*
