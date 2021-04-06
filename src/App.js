4f8f7b4d66de222614ff51b7aebf1d0c4aeb35b7
aEmreAydemir <45498427+aEmreAydemir@users.noreply.github.com>
2021-04-07 01:56:24 +0300

    Update App.js
    if no robots could be fetched yet, there will be a loading screen rather than searchbox and robot list

--- src/App.js
+++ src/App.js
@@ -32,13 +32,19 @@ class App extends React.Component {
     const filteredRobots = this.state.robots.filter(robots =>{
         return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
     })
-    return (
+    
+    if(this.state.robots.length===0) {
+        return <h1 className='tc'>Loading</h1>
+    }else {
+       return (
     <div className='tc'>
     <h1>RoboFriends</h1>
     <SearchBox searchChange={this.onSearchChange} />
     <CardList robots={filteredRobots}/>
     </div>
     ) 
+    }
+     
  }
     
 }
