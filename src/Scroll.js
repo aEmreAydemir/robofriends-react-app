3be115836c04423fa636aec9bc2a395e0d6d2b64
aEmreAydemir <45498427+aEmreAydemir@users.noreply.github.com>
2021-04-07 02:49:47 +0300

    Fix Scroll.js
    fixed dumb mistake caused by accidentally deleting 'p' from 1000px

--- src/Scroll.js
+++ src/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Scroll =(props) => {
 return (
-    <div style={{overflowY:'scroll',border:'1px solid black',height:'1000x'}}>
+    <div style={{overflowY:'scroll',height:'1000px',border:'1px solid black' }}>
         {props.children}
     </div>
 )
